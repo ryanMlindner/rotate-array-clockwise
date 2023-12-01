1d0f277ed388e763aca7f70cc440b2e62ce8c7da
RyanLindner <ryan.m.lindner@gmail.com>
2023-12-01 13:47:42 -0700

    pseudocode, solution

--- javascript/rotate_array.js
+++ javascript/rotate_array.js
@@ -1,5 +1,16 @@
 function rotateArray(arr, k) {
-  // type your code here
+  if (arr.length - k < 0) {
+    k = k % arr.length
+  }
+  let sliceIndex = arr.length - k;
+  if (sliceIndex === 0) {
+    return arr
+  }
+  rightHalf = arr.slice(sliceIndex, arr.length);
+  leftHalf = arr.slice(0, sliceIndex);
+  newArray = rightHalf + ',' + leftHalf;
+  return newArray;
+
 }
 
 if (require.main === module) {
@@ -21,4 +32,16 @@ if (require.main === module) {
 module.exports = rotateArray;
 
 // Please add your pseudocode to this file
+/**
+ * given array A, rotate the values of the array rightward by K units
+ * check that k is smaller than length of A, if not then divide k by A, take the remainder
+ * then store k values in a subarray B, add the values of the rest of the array A to the end
+ * of subarray B and return 
+ */
 // And a written explanation of your solution
+/**
+ * taking an array and rotating it clockwise is simpler to do on paper than by computer, but not by much
+ * just take the amount that you want to rotate the array by, splice it at that index (from the back, not front)
+ * and put the spliced array in front of the remainder of the array
+ * in the case that the array length and the amount to rotate by is the same, return the array unchanged
+ */
